b6c3c85157d309eaef3109444cab602c20ee083c
Ami Onodera <onodera.camila@gmail.com>
2020-11-21 18:43:36 +0100

    search working

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
 const apiKey = 'api_key=OAyL35F8mcAF0ba3i5nxeBgTVzbw9FW4';
 
+let trendElement = document.getElementById("trendResults");
+let randomElement = document.getElementById("randomResult");
+let searchElement = document.getElementById("searchResults");
+
 // get trending results
 
 async function getTrends() {
@@ -17,16 +21,16 @@ function createGifGrid(data) {
 }
 
 async function displayTrends() {
-    let resultElement = document.getElementById("trendResults");
-    let randomElement = document.getElementById("randomResult")
 
     // limpa a área de trabalho antes de mostrar novos resultados
+    trendElement.innerHTML = "";
     randomElement.innerHTML = "";
+    searchElement.innerHTML = "";
 
     const result = await getTrends();
     for (let i = 0; i <result.data.length; i++) {
         let trendingGif = createGifGrid(result.data[i]);
-        resultElement.appendChild(trendingGif)
+        trendElement.appendChild(trendingGif)
     }
 }
 
@@ -40,41 +44,56 @@ async function getRandom() {
 }
 
 async function displayRandom() {
-    let trendElement = document.getElementById("trendResults")
-    let resultElement = document.getElementById("randomResult");
 
     const result = await getRandom()
 
     // limpa a área de trabalho antes de mostrar novos resultados
     trendElement.innerHTML = "";
-    resultElement.innerHTML = "";
+    randomElement.innerHTML = "";
+    searchElement.innerHTML = "";
 
-    let randomElement = document.createElement("img");
-    randomElement.src = result.data.images.original.url;
-    resultElement.appendChild(randomElement)
+    let newRandomElement = document.createElement("img");
+    newRandomElement.src = result.data.images.original.url;
+
+    randomElement.appendChild(newRandomElement)
 
 }
 
 // clear workspace
 
 function clearSearch() {
-    let trendElement = document.getElementById("trendResults");
-    let randomElement = document.getElementById("randomResult")
-
     trendElement.innerHTML = "";
     randomElement.innerHTML = "";
+    searchElement.innerHTML = "";
 }
 
 
-// search 
+// search results
 
-// async function searchGif() {
-//     // fetch API response
-//   let search = document.getElementById("search");
-//   let searchCountry = search.value;
-//   let url = `https://restcountries.eu/rest/v2/name/${searchCountry}`;
+async function searchGif() {
+    
+  let search = document.getElementById("search-box");
+  let searchGif = search.value;
+  let searchUrl = `https://api.giphy.com/v1/gifs/search?q=${searchGif}&${apiKey}&limit=20`;
 
-//   let response = await fetch(url);
-//   let result = await response.json();
+  const response = await fetch(searchUrl);
+  const result = await response.json();
+  return result
+}
 
-// }
+async function displaySearch() {
+
+    let search = document.getElementById("search-box");
+        
+    // limpa a área de trabalho antes de mostrar novos resultados
+    randomElement.innerHTML = "";
+    trendElement.innerHTML = "";
+    searchElement.innerHTML = "";
+    
+    const result = await searchGif();
+    for (let i = 0; i <result.data.length; i++) {
+        let searchedGif = createGifGrid(result.data[i]);
+        searchElement.appendChild(searchedGif)
+    }
+    search.value = "";
+}
