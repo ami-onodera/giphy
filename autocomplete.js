1b739f9aaff38be36336a620fe1d2b67dd8ba77c
Ami Onodera <onodera.camila@gmail.com>
2020-11-26 17:47:50 +0100

    autocomplete in progress

---
+++ autocomplete.js
@@ -0,0 +1,33 @@
+let searchInput = document.getElementById("search-box");
+const autocompleteContainer = document.getElementById("list") 
+
+async function getAutoComplete(word) {
+
+    autocompleteContainer.innerHTML = "";
+
+    if (word === "") {
+        return;
+    }
+    console.log(word);
+    
+  let searchGif = word;
+  let searchUrl = `https://api.giphy.com/v1/gifs/search/tags?q=${searchGif}&${apiKey}&limit=24`;
+
+  const response = await fetch(searchUrl);
+  const result = await response.json();
+  console.log(result);
+
+
+    for (let i = 0; i < 5; i++) {
+        let suggestion = result.data[i].name
+        let suggestionBox = document.createElement("li");
+
+        suggestionBox.innerText = suggestion
+
+        autocompleteContainer.appendChild(suggestionBox)   
+    }
+
+    // suggestionBox.innerHTML = "";
+
+  return result
+}
